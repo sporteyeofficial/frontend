ac81659815d4fac6607f41d073514b1558a8ebf5
sporteyeofficial <sporteyeofficial@hotmail.com>
2023-12-16 14:10:29 +0100

    ng build 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,7 +22,7 @@ import { VeranderWachtwoordComponent } from './veranderWachtwoordModal';
 import { PasswordComponent } from './html/password';
 import { AlgVoorwaardenComponent } from './html/algvoorwaarden';
 import { ChatBotComponent } from './chatbotwindow';
-import { APP_BASE_HREF, CommonModule, HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { APP_BASE_HREF, CommonModule} from '@angular/common';
 import { ShopcartComponent } from './html/shop-cart';
 
 @NgModule({
