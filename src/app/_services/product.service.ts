f552dc13b88c318d1d6c37c3d13b101d753b9e67
sporteyeofficial <sporteyeofficial@hotmail.com>
2023-12-23 11:13:27 +0100

    fix calls

--- src/app/_services/product.service.ts
+++ src/app/_services/product.service.ts
@@ -4,28 +4,29 @@ import { Observable } from "rxjs";
 import { backend } from "src/environments";
 
 const httpOptions = {
-    headers: new HttpHeaders({ 'Content-Type': 'application/json'})
+    headers: new HttpHeaders({ 'Content-Type': 'application/json'}
+    )
   };
 
 @Injectable({
     providedIn: 'root'
   })
   export class ProductService {
-    private apiUrl = backend.url;
+    private apiUrl = "https://sporteye-backend-987b3e5bb000.herokuapp.com/api";
 
   
     constructor(private http: HttpClient) { }
 
     getProducts() : Observable<any>{
-        return this.http.get(`${this.apiUrl}/auth/product`, { withCredentials: true });
+        return this.http.get(`${this.apiUrl}/auth/product`, httpOptions);
     }
 
     fetchShirts() : Observable<any> {
-      return this.http.get(`${this.apiUrl}/user/shirts`, httpOptions);
+      return this.http.get(`${this.apiUrl}/user/shirts`, { withCredentials: true });
     }
 
     fetchClubs() : Observable<any> {
-      return this.http.get(`${this.apiUrl}/user/clubs`, httpOptions);
+      return this.http.get(`${this.apiUrl}/user/clubs`, { withCredentials: true });
     }
 
     profielClubs() : Observable<any> {
