fd1011e5aa6165eca4f7440c12c16550ac506b8e
sporteyeofficial <sporteyeofficial@hotmail.com>
2024-04-21 11:22:09 +0200

    environment change

--- src/environments.ts
+++ src/environments.ts
@@ -1,11 +1,11 @@
 export const backend = {
-    url: "http://localhost:8080/api",
+    url: "https://api.sports-eye.be/api",
     produrl: "https://api.sports-eye.be/api",
     testurl: "http://localhost:8080/api"
 }
 
 export const domain = {
-    url: 'localhost',
+    url: 'Sports-Eye.be',
     prod: 'Sports-Eye.be',
     test: 'localhost'
 }
