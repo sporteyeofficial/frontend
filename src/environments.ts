d836310778c7c6a4f0c31729d69fec8b94345cab
sporteyeofficial <sporteyeofficial@hotmail.com>
2024-05-22 21:07:58 +0200

    Change to prod

--- src/environments.ts
+++ src/environments.ts
@@ -1,12 +1,12 @@
 export const backend = {
-    url: "http://localhost:8080/api",
+    url: "https://api.sports-eye.be/api",
     produrl: "https://api.sports-eye.be/api",
     testonl: "https://testapi.sports-eye.be/api",
     testurl: "http://localhost:8080/api"
 }
 
 export const domain = {
-    url: 'localhost',
+    url: 'Sports-Eye.be',
     prod: 'Sports-Eye.be',
     testonl: 'testing.sports-eye.be',
     test: 'localhost'
